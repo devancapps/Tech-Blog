73b0c9780418752186d76d1fde10491103ce1b49
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-12-06 19:20:37 -0600

    Route updates and readme

--- Main/controllers/api/userRoutes.js
+++ Main/controllers/api/userRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({username: req.body.name, email: req.body.email, password: req.body.password});
--- Main/models/Post.js
+++ Main/models/Post.js
@@ -49,39 +49,3 @@ module.exports = (sequelize, DataTypes) => {
 
     return Post;
 };
-
-
-
-// module.exports = (sequelize, DataTypes) => {
-//     const Post = sequelize.define('Post', {
-//         id: {
-//             type: DataTypes.INTEGER,
-//             primaryKey: true,
-//             autoIncrement: true
-//         },
-//         title: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-//         content: {
-//             type: DataTypes.TEXT,
-//             allowNull: false
-//         }
-//     }, {
-//         timestamps: true,
-//         freezeTableName: true
-//     });
-
-//     Post.associate = (models) => {
-//         // associations can be defined here
-//         Post.belongsTo(models.User, {
-//             foreignKey: 'user_id',
-//             onDelete: 'CASCADE'
-//         });
-//         Post.hasMany(models.Comment, {
-//             foreignKey: 'post_id'
-//         });
-//     };
-
-//     return Post;
-// };
